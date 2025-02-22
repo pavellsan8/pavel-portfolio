0ea565cfa56d65baf462574629621ab5389194df
pavellsan8 <pavelryan146@gmail.com>
2025-02-22 16:20:55 +0700

    Adjust footer height

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className="bg-custom-color_2 text-white py-5 md:py-8">
+      <footer className="bg-custom-color_2 text-white py-5 md:py-7">
         <div className="container px-5 md:px-20 flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
           <div className="text-center md:text-left">
             <p className="text-sm text-gray-400 mt-1">
